3e46bfb826052601a40ee6395d7b10aee0fbf101
Budding Techy <96475520+Achloo@users.noreply.github.com>
2022-02-04 15:56:02 +0530

    Create main.py

---
+++ main.py
@@ -0,0 +1,139 @@
+# Author: Fayas (https://github.com/FayasNoushad) (@FayasNoushad)
+
+import os
+
+from pyrogram import Client, filters
+
+from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton
+
+START_TEXT = """
+
+<b>Hello {},
+
+I am a simple youtube thumbnail downloader telegram bot.</b>
+
+- <code>Send a youtube video link or video ID.</code>
+
+- <code>I will send the thumbnail.</code>
+
+<b>Made by @Mo_Tech_YT</b>
+
+"""
+
+START_BUTTON = InlineKeyboardMarkup(
+
+        [[
+
+        InlineKeyboardButton('🗣️ Group', url='https://telegram.me/Mo_Tech_Group'),
+
+        InlineKeyboardButton('📢 Channel', url='https://telegram.me/Mo_Tech_YT'),
+
+        InlineKeyboardButton('📃 Bot List', url='https://t.me/Mo_Tech_YT/176')
+
+        ],[
+
+        InlineKeyboardButton('🔻 Subscribe Now YouTube 🔻', url='https://youtube.com/channel/UCmGBpXoM-OEm-FacOccVKgQ')
+
+        ]]
+
+    )
+
+BUTTONS = InlineKeyboardMarkup(
+
+        [[
+
+        InlineKeyboardButton('🔻 Subscribe Now YouTube 🔻', url='https://youtube.com/channel/UCmGBpXoM-OEm-FacOccVKgQ')
+
+        ]]
+
+    )
+
+REGEX = r"^((?:https?:)?\/\/)?((?:www|m)\.)?((?:youtube\.com|youtu.be))(\/(?:[\w\-]+\?v=|embed\/|v\/)?)([\w\-]+)(\S+)?$"
+
+Bot = Client(
+
+    "YouTube-Thumbnail-Downloader",
+
+    bot_token = os.environ["BOT_TOKEN"],
+
+    api_id = int(os.environ["API_ID"]),
+
+    api_hash = os.environ["API_HASH"]
+
+)
+
+@Bot.on_message(filters.private & filters.command(["start"]))
+
+async def start(bot, update):
+
+    text = START_TEXT.format(update.from_user.mention)
+
+    reply_markup = START_BUTTON
+
+    await update.reply_text(
+
+        text=text,
+
+        disable_web_page_preview=True,
+
+        reply_markup=reply_markup,
+
+        quote=True
+
+    )
+
+@Bot.on_message(filters.private & filters.text)
+
+async def send_thumbnail(bot, update):
+
+    message = await update.reply_text(
+
+        text="`Analysing...`",
+
+        disable_web_page_preview=True,
+
+        quote=True
+
+    )
+
+    if ("youtube.com" in update.text) and ("/" in update.text) and ("=" in update.text):
+
+        id = update.text.split("=")[-1]
+
+    elif ("youtu.be" in update.text) and ("/" in update.text):
+
+        id = update.text.split("/")[-1]
+
+    else:
+
+        id = update.text
+
+    try:
+
+        thumbnail = "https://img.youtube.com/vi/" + id + "/sddefault.jpg"
+
+        await update.reply_photo(
+
+            photo=thumbnail,
+
+            reply_markup=BUTTONS,
+
+            quote=True
+
+        )
+
+        await message.delete()
+
+    except Exception as error:
+
+        await message.edit_text(
+
+            text=error,
+
+            disable_web_page_preview=True,
+
+            reply_markup=BUTTONS
+
+        )
+
+Bot.run()
